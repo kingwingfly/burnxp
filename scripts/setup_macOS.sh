274d36b64961d82001258b972b93894ef8d878d6
Louis <836250617@qq.com>
2024-09-12 10:49:24 +0800

    fix ci macOS

--- scripts/setup_macOS.sh
+++ scripts/setup_macOS.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-python3 -m venv .venv
-source .venv/bin/activate
+python3 -m venv venv
+source venv/bin/activate
 pip install torch==2.2.0 numpy==1.26.4 setuptools
 
 mkdir .cargo
@@ -9,7 +9,7 @@ mkdir .cargo
 cat <<EOF > .cargo/config.toml
 [env]
 LIBTORCH_USE_PYTORCH = "1"
-DYLD_LIBRARY_PATH = "$(pwd)/$(find .venv -type d -name "lib" | grep /torch):$DYLD_LIBRARY_PATH"
+DYLD_LIBRARY_PATH = "$(pwd)/$(find venv -type d -name "lib" | grep /torch):$DYLD_LIBRARY_PATH"
 EOF
 
 deactivate

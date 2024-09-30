fab14c31d3fc6c28e4f2e7256106eb7bc7cfae8a
Louis <836250617@qq.com>
2024-09-30 18:16:40 +0800

    improve: grace and efficient Image Grid

--- tagger/src/components/image.rs
+++ tagger/src/components/image.rs
@@ -1,14 +1,14 @@
 use crate::components::Title;
 use anyhow::{anyhow, Result};
-use image::DynamicImage;
 use ratatui::{
     buffer::Buffer,
     layout::{Constraint, Direction, Layout, Rect},
     style::{Color, Style},
-    widgets::{Block, Borders, StatefulWidget, Widget},
+    widgets::{Block, Borders, Widget},
 };
-use ratatui_image::{picker::Picker, FilterType, Resize, StatefulImage};
+use ratatui_image::{picker::Picker, protocol::StatefulProtocol, FilterType, Resize};
 use std::path::PathBuf;
+use std::sync::mpsc::{channel, Receiver};
 use std::sync::{Arc, RwLock};
 use std::thread;
 
@@ -43,19 +43,16 @@ impl<'a> Widget for Images<'a> {
             .direction(Direction::Horizontal)
             .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
             .split(area);
-        let mut jhs = Vec::with_capacity(2);
+        let mut images = Vec::with_capacity(2);
         for i in 0..2 {
-            let path = self.paths[i].clone();
-            let picker = self.picker.clone();
-            jhs.push(thread::spawn(move || -> Result<Image> {
-                let image = image::open(&path)?;
-                Ok(Image::new(picker, image, path))
-            }))
+            images.push(Image::new(
+                self.picker.clone(),
+                self.paths[i].clone(),
+                chunks[i],
+            ));
         }
-        for (jh, &chunk) in jhs.into_iter().zip(chunks.iter()) {
-            if let Ok(Ok(w)) = jh.join() {
-                w.render(chunk, buf);
-            }
+        for (i, image) in images.into_iter().enumerate() {
+            image.render(chunks[i], buf);
         }
     }
 }
@@ -111,70 +108,61 @@ impl<'a> Widget for Grid<'a> {
                     .cloned()
                     .collect::<Vec<_>>()
             })
+            .enumerate()
+            .map(|(i, chunk)| {
+                let block = Block::default()
+                    .title(format!("{}", i + 1))
+                    .borders(Borders::ALL)
+                    .border_style(Style::default().fg(match self.heighlight[i] {
+                        true => Color::Green,
+                        false => Color::White,
+                    }));
+                let inner = block.inner(chunk);
+                block.render(chunk, buf);
+                inner
+            })
             .collect::<Vec<_>>();
-        let mut jhs = Vec::with_capacity(9);
-        for path in self.paths {
-            let path = path.clone();
-            let picker = self.picker.clone();
-            jhs.push(thread::spawn(move || -> Result<Image> {
-                let image = image::open(&path)?;
-                Ok(Image::new(picker, image, path.clone()))
-            }))
+        let mut images = Vec::with_capacity(9);
+        for (i, path) in self.paths.iter().enumerate() {
+            images.push(Image::new(self.picker.clone(), path.clone(), grid[i]));
         }
-        for (i, jh) in jhs.into_iter().enumerate() {
-            let block = Block::default()
-                .title(format!("{}", i + 1))
-                .borders(Borders::ALL)
-                .border_style(Style::default().fg(match self.heighlight[i] {
-                    true => Color::Green,
-                    false => Color::White,
-                }));
-            let inner = block.inner(grid[i]);
-            block.render(grid[i], buf);
-            if let Ok(Ok(w)) = jh.join() {
-                w.render(inner, buf);
-            }
+        for (i, image) in images.into_iter().enumerate() {
+            image.render(grid[i], buf);
         }
     }
 }
 
 struct Image {
-    picker: Arc<RwLock<Picker>>,
-    image: DynamicImage,
-    path: PathBuf,
+    rx: Receiver<Box<dyn StatefulProtocol>>,
 }
 
 impl Image {
-    pub(crate) fn new(picker: Arc<RwLock<Picker>>, image: DynamicImage, path: PathBuf) -> Self {
-        Self {
-            picker,
-            image,
-            path,
-        }
+    pub(crate) fn new(picker: Arc<RwLock<Picker>>, path: PathBuf, chunk: Rect) -> Self {
+        let (tx, rx) = channel::<Box<dyn StatefulProtocol>>();
+        thread::spawn(move || -> Result<()> {
+            let image = image::open(path)?;
+            let mut image_fit_state = {
+                let mut picker = picker
+                    .write()
+                    .map_err(|_| anyhow!("Race condition of Picker RwLock"))?;
+                picker.new_resize_protocol(image)
+            };
+            image_fit_state.resize_encode(&Resize::Fit(Some(FilterType::Triangle)), None, chunk);
+            tx.send(image_fit_state)?;
+            Ok(())
+        });
+        Self { rx }
     }
 }
 
 impl Widget for Image {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        fn inner(this: Image, area: Rect, buf: &mut Buffer) -> Result<()> {
-            let mut picker = this
-                .picker
-                .write()
-                .map_err(|_| anyhow!("Race condition of Picker RwLock"))?;
-            let mut image_fit_state = picker.new_resize_protocol(this.image);
-            let image = StatefulImage::new(None).resize(Resize::Fit(Some(FilterType::Nearest)));
-            image.render(area, buf, &mut image_fit_state);
-            Ok(())
+        match self.rx.recv() {
+            Ok(mut protocol) => protocol.render(area, buf),
+            Err(e) => Title {
+                title: format!("Failed to render image: {e}"),
+            }
+            .render(area, buf),
         }
-        let path = self.path.clone();
-        inner(self, area, buf)
-            .or_else(|e| -> Result<()> {
-                Title {
-                    title: format!("{}: {}", path.display(), e),
-                }
-                .render(area, buf);
-                Ok(())
-            })
-            .ok();
     }
 }
